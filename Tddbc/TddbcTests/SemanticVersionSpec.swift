0838fd6386963236f1dce9e680d8f05b1de5365b
keygx <y.kagiyama@gmail.com>
2017-09-30 14:03:10 +0900

    Add Quick

---
+++ Tddbc/TddbcTests/SemanticVersionSpec.swift
@@ -0,0 +1,16 @@
+//
+//  SemanticVersionSpec.swift
+//  TddbcTests
+//
+//  Created by keygx on 2017/09/30.
+//  Copyright © 2017年 gaopin. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+
+class SemanticVersionSpec: QuickSpec {
+    override func spec() {
+    }
+}
