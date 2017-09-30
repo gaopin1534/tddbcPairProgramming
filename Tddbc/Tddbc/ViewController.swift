8f0edc710e5c19b1cc09cf9aba1e0f223bac91c2
高松幸平 <gaoxin.xixxix@gmail.com>
2017-09-30 13:26:33 +0900

    initial cmmit

---
+++ Tddbc/Tddbc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tddbc
+//
+//  Created by 高松幸平 on 2017/09/30.
+//  Copyright © 2017年 gaopin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
