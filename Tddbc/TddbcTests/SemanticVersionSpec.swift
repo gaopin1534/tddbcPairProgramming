e115efcf336578349a04f0c0fcf294deaafc1b50
高松幸平 <gaoxin.xixxix@gmail.com>
2017-09-30 15:31:30 +0900

    make it equatable

--- Tddbc/TddbcTests/SemanticVersionSpec.swift
+++ Tddbc/TddbcTests/SemanticVersionSpec.swift
@@ -30,10 +30,38 @@ class SemanticVersionSpec: QuickSpec {
                 expect(actual.toString()).to(equal("1.2.3"))
             }
         }
+        
+        describe("等価判定") {
+            context("同じバージョンを持つオブジェクトを比較したとき") {
+                let major = 1
+                let minor = 2
+                let patch = 3
+                let left = SemanticVersion(major: major, minor: minor, patch: patch)
+                let right = SemanticVersion(major: major, minor: minor, patch: patch)
+                
+                
+                it("trueを返す") {
+                    expect(left).to(equal(right))
+                }
+            }
+            
+            context("異なるバージョンを持つオブジェクトを比較したとき") {
+                let major = 1
+                let minor = 2
+                let leftPatch = 3
+                let rightPatch = 5
+                let left = SemanticVersion(major: major, minor: minor, patch: leftPatch)
+                let right = SemanticVersion(major: major, minor: minor, patch: rightPatch)
+                
+                it("falseを返す") {
+                    expect(left).toNot(equal(right))
+                }
+            }
+        }
     }
 }
 
-struct SemanticVersion {
+struct SemanticVersion: Equatable{
     var major: Int
     var minor: Int
     var patch: Int
@@ -41,4 +69,8 @@ struct SemanticVersion {
     func toString() -> String {
         return "\(major).\(minor).\(patch)"
     }
+}
+
+func ==(lhs: SemanticVersion, rhs: SemanticVersion) -> Bool {
+    return lhs.major == rhs.major && lhs.minor == rhs.minor && lhs.patch == rghs.patch
 }
