531a382a971a896c74d807e550d7fa017788cf5f
高松幸平 <gaoxin.xixxix@gmail.com>
2017-09-30 14:21:22 +0900

    add initialization

--- Tddbc/TddbcTests/SemanticVersionSpec.swift
+++ Tddbc/TddbcTests/SemanticVersionSpec.swift
@@ -12,5 +12,22 @@ import Nimble
 
 class SemanticVersionSpec: QuickSpec {
     override func spec() {
+        describe("initialize") {
+            context("initializerにmajor,minor,patchの引数が渡されたとき") {
+                let major = 1
+                let minor = 2
+                let patch = 3
+                let target = SemanticVersion(major: major, minor: minor, patch: patch)
+                it("SemanticVErsionインスタンスが生成される") {
+                    expect(type(of: target)).notTo(beNil())
+                }
+            }
+        }
     }
+}
+
+struct SemanticVersion {
+    var major: Int
+    var minor: Int
+    var patch: Int
 }
